d8206c30f4c90ccdbb1c874c9ad808f510cb5efd
Varun Kushwah <varun.kush3@gmail.com>
2026-02-19 01:03:34 +0530

    Created PatientRepository.

---
+++ patient-service/src/main/java/com/pm/patient_service/Repository/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.pm.patient_service.Repository;
+
+import com.pm.patient_service.Model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+}
